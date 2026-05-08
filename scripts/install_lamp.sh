19aa7995ca999bc262b1129b89a3ba6d5314b5b6
Marina <mtamaralg@ip-172-31-37-164.ec2.internal>
2026-05-08 11:43:13 +0000

    practica hecha

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -6,6 +6,9 @@ sudo apt update
 #Instalar apache
 sudo apt install apache2 -y
 
+#Copiamos archivo de configuracion de apache
+cp ../conf/000-default.conf /etc/apache2/sites-available/
+
 #Instalar My SQL
 sudo apt install mysql-server -y
 
@@ -14,3 +17,7 @@ sudo apt install php libapache2-mod-php php-mysql -y
 
 #Reiniciar servicio de Apache
 sudo systemctl restart apache2
+
+# Copiamos el archivo index.php en /var/www/html
+cp ../php/index.php /var/www/html
+
